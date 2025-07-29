cdba9cd337b44c00649c0baf639b68b83008114e
Koki Nakamura <koki-nakamura@telema.jp>
2025-07-29 15:17:55 +0900

    new_config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-northeast-1_9DNcB9Ny5", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "4pfbn2k1noudu4h11uaaul5ie9", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
